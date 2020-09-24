72656d4dfcafc30ae5f7aec4beb5a4f271e3df2d
privet11 <50716878+privet11@users.noreply.github.com>
2020-09-24 17:58:17 +0300

    Update EntertainmentArtsPage.cs

--- Automation_Home_Project/Pages/EntertainmentArtsPage.cs
+++ Automation_Home_Project/Pages/EntertainmentArtsPage.cs
@@ -13,7 +13,10 @@ namespace Automation_Home_Project.PageObject
         private readonly IWebElement headlinesList = WebDriver.FindElement(By.XPath("//div[contains(@class,'3@xxl')]//a[contains(@class,'gs-c-promo')]"));
         //a[contains(@class,'gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-paragon-bold gs-u-mt+ nw-o-link-split__anchor')]
 
-        public void FirstHeadlineClick() => headlinesList.Click();
+          public void FirstHeadlineClick() 
+        { 
+            headlinesList.Where(x=> x.Displayed).FirstOrDefault().Click(); 
+        }
 
         
 
